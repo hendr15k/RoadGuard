8b4ba32f4d20aa22ee011e7b89d60b7cf5ac8411
hendr15k <hendrik.schmitz03.sh@gmail.com>
2026-09-13 00:14:30 +0200

    chore: bump to v1.0.64 [skip ci]

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.roadguard.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 23
-        versionName = "v1.0.63"
+        versionCode = 24
+        versionName = "v1.0.64"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
